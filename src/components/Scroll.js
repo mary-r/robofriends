de86c60520298a6d242e8edb907ba4df7882fb4e
Mary Romeral <maryromeral@Marys-MacBook-Pro.local>
2020-05-08 06:39:43 -0400

    Improved layout

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', border: '#02050D solid 15px', backgroundColor: '#F2F3F2', height: '700px', width: '75%', margin: 'auto' }}>
+    <div style={{ overflow: 'scroll', border: '#02050D solid 15px', backgroundColor: '#F2F3F2', height: '700px', width: '75%', margin: 'auto' }}>
             {props.children}
         </div>
     )
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
     return (
-        <div className='pa2'>
+        <div className='pt2 pb4'>
             <input 
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search' 
